171acd57a0ae12cbe0f8aaa7d8c29570db7d9002
Fabian <fabianres@live.com.mx>
2015-12-15 12:28:01 -0600

    aws improvement3

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,5 +3,8 @@ has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/,
 :download,
 :storage => :s3,
-:s3_credentials => Rails.root.join("config/aws.yml")
+:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+	def s3_credentials
+		{:bucket => "fabian-railsauth-assets", :access_key_id => "AKIAIFT64EEKP2HMDKUQ", :secret_access_key => "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"}
+	end
 end
