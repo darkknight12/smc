c484c45db3cf402fa813cbc8fc70a2573b048bda
Fabian <fabianres@live.com.mx>
2015-12-15 12:33:46 -0600

    aws improvement4

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,4 @@
 class Listing < ActiveRecord::Base
 has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
-validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/,
-:download,
-:storage => :s3,
-:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-	def s3_credentials
-		{:bucket => "fabian-railsauth-assets", :access_key_id => "AKIAIFT64EEKP2HMDKUQ", :secret_access_key => "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"}
-	end
+validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
