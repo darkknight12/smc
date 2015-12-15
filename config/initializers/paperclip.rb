bd1567bf22413515890f47669b341c87fb21dae2
Fabian <fabianres@live.com.mx>
2015-12-15 17:40:03 -0600

    aws improvement_16

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,9 @@
+# only use Amazon S2 on production servers
+unless Rails.env.development?
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+end
