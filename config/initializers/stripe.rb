cb58c88facf8733646a385560c427aee7bc06713
Fabian <fabianres@live.com.mx>
2015-12-20 15:59:21 -0600

    delete stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,7 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['stripe_publishable_key'],
-  :secret_key      => ENV['stripe_api_key']
-}
-
-#Stripe.api_key = Rails.configuration.stripe[:secret_key]
-Stripe.api_key = 'sk_test_CUsoGvy6un4BVpgG6vMMVr94'
