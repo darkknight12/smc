70b66d32dc6a43fede1e6e7a54010d738dfa40f6
Fabian <fabianres@live.com.mx>
2015-12-20 15:46:53 -0600

    Modifications in stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,5 @@ Rails.configuration.stripe = {
   :secret_key      => ENV['stripe_api_key']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = 'sk_test_CUsoGvy6un4BVpgG6vMMVr94'
