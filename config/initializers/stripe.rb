c41184002b31b7eafbac1e49bc9fecb66ddc93ff
Fabian <fabianres@live.com.mx>
2015-12-21 00:55:49 -0600

    Damn

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = { 
-  :publishable_key => Rails.application.secrets.publishable_key, 
-  :secret_key => Rails.application.secrets.secret_key
+  :publishable_key => 'pk_test_qN9mOmZV0e2wAFRNpbIPvn6u', 
+  :secret_key => 'sk_test_CUsoGvy6un4BVpgG6vMMVr94'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
